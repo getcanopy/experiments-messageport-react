656580adb9ab141c7b8305e08a984436b0461e72
redaphid <iam@aaronherres.com>
2022-09-16 02:13:23 -0700

    have example show off different properties changing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
-import {PortPortal} from './PortPortal'
+import { PortPortal } from './PortPortal'
+
 const HappyGreeting = ({ greeting }) => {
   return <div>
     <span>😀</span>
@@ -9,18 +10,27 @@ const HappyGreeting = ({ greeting }) => {
   </div>
 }
 
-
-const {port1, port2} = new MessageChannel()
+const SomethingElse = ({ bgColor, onClick }) => {
+  return <div style={{ backgroundColor: bgColor }}>
+    <button onClick={onClick}>Click me!</button>
+  </div>
+}
+const { port1, port2 } = new MessageChannel()
 
 setInterval(() => {
-  port2.postMessage({greeting: `hello from time: ${Date.now()}`})
+  port2.postMessage({ greeting: `hello from time: ${Date.now()}` })
 }, 100)
+
+setInterval(() => {
+  port2.postMessage({ bgColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})` })
+}, 1000)
 
 function App() {
   return (
     <div className="App">
       <PortPortal port={port1}>
         <HappyGreeting greeting='no greeting' />
+        <SomethingElse bgColor='blue' onClick={() => console.log('clicked')} />
       </PortPortal>
     </div>
   )
